7b8c9b30332ae3a44afe309f78c42e1df5400d8d
Nikita Kharlap <harlap19@gmail.com>
2022-04-24 22:14:24 +0300

    add table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export interface IData {
+  id: string,
+  fio: string,
+  address: string,
+  phone: string,
+}
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit, OnDestroy {
+  data: IData[] = [];
 
   constructor(private router: Router) {
+    for (let i = 0; i < 20; i++) {
+      this.data.push({ id: '1', fio: 'fio', address: 'address', phone: 'phone' });
+    }
   }
 
   ngOnInit(): void {
